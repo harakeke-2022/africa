c009476c21496076f78eeaaef0e8141e6a944f73
Angela Yen <89116466+angela-yen@users.noreply.github.com>
2022-02-03 16:50:23 +1300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const routes = require('./routes/index')
+
+const server = express()
+
+server.use(express.json())
+server.use(express.static(path.join(__dirname, './public')))
+server.use('/api/v1/example', routes)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+module.exports = server
